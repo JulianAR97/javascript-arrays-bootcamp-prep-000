4c32484905f641b0a7dbe3fef7f1f2f54abdb862
whimsical-logger-3153 <julianrosenthal@yahoo.com>
2020-08-25 23:13:48 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,19 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+
 function addElementToBeginningOfArray(arr, ele) {
   return [ele, ...arr]
 }
 
 function destructivelyAddElementToBeginningOfArray(arr, ele) {
-  return arr.shift(ele);
+  arr.shift(ele);
+  return arr; 
+}
+
+function addElementToEndOfArray(arr, ele) {
+  return [...arr, ele];
+}
+
+function destructivelyAddElementFromEndOfArray(arr, ele) {
+  arr.push(ele);
+  return arr; 
 }
